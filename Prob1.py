e6ebdaeb19a13ba8350c65c7d0ea302ab5f5577c
McBxby <mlcrandall@willamette.edu>
2024-10-25 10:11:39 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -8,34 +8,32 @@ from pgl import *
 
 #1a
 def create_histogram_array(data:list[int])->list[int]:
-    hist = [0] * 10  # Create list of 10 zeros for digits 0-9
-    for num in data:
-        hist[num] += 1  # Increment count for each digit
+    hist = [0] * (max(data) + 1)
+    for value in data:
+        hist[value] += 1
     return hist
-
 #1b
 def print_histogram(hist:list[int]) -> None:
-    for i in range(len(hist)):
-        if hist[i] > 0:
-            print(f"{i}: {'*' * hist[i]}")
-
+    [print(str(i) + ": " + hist[i] * "*") for i in range(len(hist))]
 #1c
 def graph_histogram(hist:list[int], width:int, height:int) -> None:
     gw = GWindow(width, height)
-    max_value = max(hist)
-    bar_width = width / len(hist)
-    scale = height / max_value
-
+    
     def my_rect(x,y,w,h,color):
         rect = GRect(x,y,w,h)
         rect.set_filled(True)
         rect.set_color(color)
         gw.add(rect)
-
+    
+    col_width = width / len(hist)
+    max_val = max(hist)
+    scale = height / max_val
+    
     for i in range(len(hist)):
-        if hist[i] > 0:
-            bar_height = hist[i] * scale
-            my_rect(i * bar_width, height - bar_height, bar_width, bar_height, "red")
+        x = i * col_width
+        h = hist[i] * scale
+        y = height - h
+        my_rect(x, y, col_width, h, "red")
 
 # Some testing printouts for your use!
 PI_DIGITS = [3,1,4,1,5,9,2,6,5,3,5,5,8,9,7,9]
