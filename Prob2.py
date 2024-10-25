b1d14a616b9165530f1048fae560b699a7f9178a
McBxby <mlcrandall@willamette.edu>
2024-10-24 18:36:43 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -26,26 +26,13 @@ def is_magic_square(array:list[list[int]]) -> bool:
     if sum(array[i][i] for i in range(n)) != expected_sum or sum(array[i][n-1-i] for i in range(n)) != expected_sum:
         return False
     
-    # Create a list of all numbers in the array
-    array_list = []
-    for row in array:
-        for num in row:
-            array_list.append(num)
-    
-    # Check if all numbers are unique
-    unique_numbers = []
-    for num in array_list:
-        if num not in unique_numbers:
-            unique_numbers.append(num)
-    if len(unique_numbers) != n * n:       
-        return False
-    
-    # Check if all numbers are between 1 and n * n
-    for num in array_list:
-        if num < 1 or num > n * n:
-            return False
-    
     return True
 
 small = [[8,1,6],[3,5,7],[4,9,2]]
+medium = [[17,24,1,8,15],[23,5,7,14,16],[4,6,13,20,22],[10,12,19,21,3],[11,18,25,2,9]]
+large = [[35,1,6,26,19,24],[3,32,7,21,23,25],[31,9,2,22,27,20],[8,28,33,17,10,15],[30,5,34,12,14,16],[4,36,29,13,18,11]]
+error = [[1,2],[3,4,5]]
 print(is_magic_square(small))
+print(is_magic_square(medium))
+print(is_magic_square(large))
+print(is_magic_square(error))
